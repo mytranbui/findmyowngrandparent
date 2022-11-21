bba8037be95ace3421fb85f8186c35c6645269cd
My Tran Bui <mytran.bui@hotmail.com>
2022-11-21 17:56:53 +0100

    lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# require "json"
+# require "open-uri"
+require "faker"
+
+puts "Cleaning database..."
+Grandparent.destroy_all
+
+# url = "https://tmdb.lewagon.com/movie/top_rated"
+# movies_serialized = URI.open(url).read
+# movies = JSON.parse(movies_serialized)
+
+puts "Creating grandparents..."
+
+100.times do
+  Grandparent.create(name: Faker::Name.name, description: Faker::TvShows::BreakingBad.episode, age: Faker::Number.number(digits: 3), user_id:1)
+end
+
+puts "Seeding Finished!"
